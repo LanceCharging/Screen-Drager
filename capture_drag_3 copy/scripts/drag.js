b53da250b8d57cfc1ff98e253e8e1a559cb5773a
JJHUNTER <worldline341@gmail.com>
2023-08-15 02:42:40 +0900

    캡처 & 드래그 & 슬라이스 95% 완성 - 슬라이싱 부분만 조금 조절하면 끝

--- capture_drag_3 copy/scripts/drag.js
+++ capture_drag_3 copy/scripts/drag.js
@@ -2,18 +2,73 @@ chrome.runtime.sendMessage({ order: "drag_start" });
 
 let startX, startY;
 
+const dragBox = document.createElement("div");
+dragBox.classList.add("drag-box");
+dragBox.style.border = "2px dashed red";
+dragBox.style.position = "absolute";
+dragBox.style.zIndex = "1000";
+
 document.addEventListener("mousedown", (e) => {
+  let dragging = true;
   startX = e.clientX;
   startY = e.clientY;
-});
 
-document.addEventListener("mouseup", (e) => {
-  const endX = e.clientX;
-  const endY = e.clientY;
+  dragBox.style.left = startX + "px";
+  dragBox.style.top = startY + "px";
+  document.body.appendChild(dragBox);
 
-  // 드래그 시작 위치와 끝 위치 정보 사용하여 작업 수행
-  chrome.runtime.sendMessage({
-    order: "message",
-    message: [startX, startY, endX, endY],
+  document.addEventListener("mousemove", (e) => {
+    if (dragging) {
+      const currentX = e.clientX;
+      const currentY = e.clientY;
+
+      const left = Math.min(startX, currentX);
+      const top = Math.min(startY, currentY);
+      const width = Math.abs(currentX - startX);
+      const height = Math.abs(currentY - startY);
+
+      dragBox.style.left = left + "px";
+      dragBox.style.top = top + "px";
+      dragBox.style.width = width + "px";
+      dragBox.style.height = height + "px";
+    }
+  });
+
+  document.addEventListener("mouseup", (e) => {
+    dragging = false;
+    const endX = e.clientX;
+    const endY = e.clientY;
+
+    chrome.runtime.sendMessage({
+      order: "message",
+      message: [startX, startY, endX, endY],
+    });
+    captureAndSaveImage();
   });
 });
+
+function captureAndSaveImage() {
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+
+  const width = parseInt(dragBox.style.width, 10);
+  const height = parseInt(dragBox.style.height, 10);
+
+  canvas.width = width;
+  canvas.height = height;
+
+  ctx.drawImage(
+    document.getElementById("capture_origin"),
+    parseInt(dragBox.style.left, 10),
+    parseInt(dragBox.style.top, 10),
+    width,
+    height,
+    0,
+    0,
+    width,
+    height
+  );
+
+  const croppedImageUrl = canvas.toDataURL("image/png");
+  chrome.runtime.sendMessage({ order: "message", message: croppedImageUrl });
+}
